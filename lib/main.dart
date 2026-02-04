062b2c687d29e15b6077b57731d1349bbb5743c5
Bashil Shrestha <bashil.shrestha@gmail.com>
2026-02-04 09:52:11 +0545

    Initial Flutter project setup with Riverpod and base structure

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: SpendWiseApp()));
+}
+
+class SpendWiseApp extends StatelessWidget {
+  const SpendWiseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SpendWise',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'SpendWise',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
